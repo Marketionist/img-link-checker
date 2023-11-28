d4cb7552965add32649008df98ea029eff16a965
Marketionist <marketionist@gmail.com>
2023-11-28 08:34:39 -0500

    Add ignore list for links

--- index.js
+++ index.js
@@ -2,22 +2,28 @@ const { promisify } = require('util');
 const fs = require('fs');
 const axios = require('axios');
 
-const checkFile = async (filePath) => {
+const checkFile = async (filePath, ignoreLinks = []) => {
     const readFile = promisify(fs.readFile);
     const text = await readFile(filePath, 'utf8');
 
-    // Filter out only links that start from http(s) and end with ) or " or '
+    // Filter out only links that start with http(s) and end with ) or " or '
     // Using negative lookahead ?! to filter out localhost:
     const links = text.match(/https?:\/\/(?!.*(localhost:)).*?[\)|"|']/gi);
 
     if (links) {
         // Clear links from last character ) or " or '
-        const clearedLinks = links.map((value) => value.slice(0, -1));
+        let filteredLinks = links.map((value) => value.slice(0, -1));
+        // Remove the ignored links if any
+        if (ignoreLinks.length > 0) {
+            filteredLinks = filteredLinks.filter((value) => {
+                return !ignoreLinks.includes(value);
+            });
+        }
 
-        console.log(`Found ${clearedLinks.length} link(s):`);
-        console.log(clearedLinks);
+        console.log(`Found ${filteredLinks.length} link(s):`);
+        console.log(filteredLinks);
 
-        const responses = await Promise.all(clearedLinks.map(async (value) => {
+        const responses = await Promise.all(filteredLinks.map(async (value) => {
             try {
                 const response = await axios.get(value);
 
@@ -31,14 +37,14 @@ const checkFile = async (filePath) => {
         const statusCodeOk = 200;
 
         responses.map((value, index) => {
-            if (value !== statusCodeOk) { brokenLinks.push(clearedLinks[index]); }
+            if (value !== statusCodeOk) { brokenLinks.push(filteredLinks[index]); }
         });
 
         if (brokenLinks.length > 0) {
             console.error('Broken links list:', brokenLinks);
             process.exit(1);
         } else {
-            return clearedLinks;
+            return filteredLinks;
         }
     } else {
         console.log(`No links found in ${filePath}`);
