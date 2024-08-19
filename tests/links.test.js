9d83ca9170f47a07e27cb6db33951efeccb415eb
Marketionist <marketionist@gmail.com>
2024-08-19 08:59:26 -0400

    Update test

--- tests/links.test.js
+++ tests/links.test.js
@@ -1,9 +1,17 @@
 const { checkFile } = require('../index.js');
 
-checkFile(
-    './README.md',
-    [
-        'https://github.com/Marketionist/img-link-checker/actions',
-        'https://www.linkedin.com/in/test/'
-    ]
-);
+(async () => {
+    const brokenLinks = await checkFile(
+        './README.md',
+        [
+            'https://github.com/Marketionist/img-link-checker/actions',
+            'https://www.linkedin.com/in/test/'
+        ]
+    );
+
+    // Print a list of broken links and exit the process (will mark the CI/CD build as failed)
+    if (brokenLinks.length > 0) {
+        console.error('Broken links list:', brokenLinks);
+        process.exit(1);
+    }
+})();
